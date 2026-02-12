b97e88164c96df07d4e21f17ed6f8688a2b48a10
Vamshi_3103 <vkovvuri@stevens.edu>
2026-02-12 17:34:17 -0500

    Initial project setup

---
+++ src/main/java/com/devhub/api_marketplace/DevHubApiMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.devhub.api_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHubApiMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHubApiMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devhub/api_marketplace/DevHubApiMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhub.api_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHubApiMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
